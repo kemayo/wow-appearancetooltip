1576632cb46fbe40532693a4c2a1046098768911
David Lynch <kemayo@gmail.com>
2016-07-21 19:13:38 -0500

    Config for custom player model

--- config.lua
+++ config.lua
@@ -11,7 +11,7 @@ local function checkboxOnClick(self)
 end
 
 local function newCheckbox(parent, key, label, description, getValue, setValue)
-    local check = CreateFrame("CheckButton", "AppearanceTooltipOptionsCheck" .. label, parent, "InterfaceOptionsCheckButtonTemplate")
+    local check = CreateFrame("CheckButton", "AppearanceTooltipOptionsCheck" .. key, parent, "InterfaceOptionsCheckButtonTemplate")
 
     check.key = key
     check.GetValue = getValue or checkboxGetValue
@@ -24,6 +24,69 @@ local function newCheckbox(parent, key, label, description, getValue, setValue)
     check.tooltipRequirement = description
     return check
 end
+
+local function newDropdown(parent, key, description, values)
+    local dropdown = CreateFrame("Frame", "AppearanceTooltipOptions" .. key .. "Dropdown", parent, "UIDropDownMenuTemplate")
+    dropdown.key = key
+    dropdown:HookScript("OnShow", function()
+        if not dropdown.initialize then
+            UIDropDownMenu_Initialize(dropdown, function(frame)
+                for k, v in pairs(values) do
+                    local info = UIDropDownMenu_CreateInfo()
+                    info.text = v
+                    info.value = k
+                    info.func = function(self)
+                        ns.db[key] = self.value
+                        UIDropDownMenu_SetSelectedValue(dropdown, self.value)
+                    end
+                    UIDropDownMenu_AddButton(info)
+                end
+            end)
+            UIDropDownMenu_SetSelectedValue(dropdown, ns.db[key])
+        end
+    end)
+    dropdown:HookScript("OnEnter", function(self)
+        if not self.isDisabled then
+            GameTooltip:SetOwner(self, "ANCHOR_TOPRIGHT")
+            GameTooltip:SetText(description, nil, nil, nil, nil, true)
+        end
+    end)
+    dropdown:HookScript("OnLeave", GameTooltip_Hide)
+    return dropdown
+end
+
+local newFontString = function(parent, text, template,  ...)
+    local label = parent:CreateFontString(nil, nil, template or 'GameFontHighlight')
+    label:SetPoint(...)
+    label:SetText(text)
+
+    return label
+end
+
+local function newBox(parent, title, height)
+    local boxBackdrop = {
+        bgFile = [[Interface\ChatFrame\ChatFrameBackground]], tile = true, tileSize = 16,
+        edgeFile = [[Interface\Tooltips\UI-Tooltip-Border]], edgeSize = 16,
+        insets = {left = 4, right = 4, top = 4, bottom = 4},
+    }
+
+    local box = CreateFrame('Frame', nil, parent)
+    box:SetBackdrop(boxBackdrop)
+    box:SetBackdropBorderColor(.3, .3, .3)
+    box:SetBackdropColor(.1, .1, .1, .5)
+
+    box:SetHeight(height)
+    box:SetPoint('LEFT', 12, 0)
+    box:SetPoint('RIGHT', -12, 0)
+
+    if title then
+        box.Title = newFontString(box, title, nil, 'BOTTOMLEFT', box, 'TOPLEFT', 6, 0)
+    end
+
+    return box
+end
+
+-- and the actual config now
 
 local panel = CreateFrame("Frame", nil, InterfaceOptionsFramePanelContainer)
 panel:Hide()
@@ -49,8 +112,35 @@ local mousescroll = newCheckbox(panel, 'mousescroll', 'Rotate with mousewheel', 
 local spin = newCheckbox(panel, 'spin', 'Spin model', "Constantly spin the model while it's displayed")
 local notifyKnown = newCheckbox(panel, 'notifyKnown', 'Display known', "Display a label showing whether you know the item appearance already")
 local currentClass = newCheckbox(panel, 'currentClass', 'Current character only', "Only show previews on items that the current character can collect")
+
 -- local zoom = newCheckbox(panel, 'zoom', 'Zoom on item', "Zoom in on the item being previewed")
 -- local  = newCheckbox(panel, 'customModel', 'Use custom model', "Choose a custom model to use instead of your current character")
+
+local modelBox = newBox(panel, "Custom player model", 48)
+local customModel = newCheckbox(modelBox, 'customModel', 'Use a different model', "Instead of your current character, use a specific race/gender")
+local customRaceDropdown = newDropdown(modelBox, 'modelRace', "Choose your custom race", {
+    [1] = "Human",
+    [3] = "Dwarf",
+    [4] = "Night Elf",
+    [11] = "Draenei",
+    [22] = "Worgen",
+    [7] = "Gnome",
+    [24] = "Pandaren",
+    [2] = "Orc",
+    [5] = "Undead",
+    [10] = "Blood Elf",
+    [8] = "Troll",
+    [6] = "Tauren",
+    [9] = "Goblin",
+})
+UIDropDownMenu_SetWidth(customRaceDropdown, 100)
+local customGenderDropdown = newDropdown(modelBox, 'modelGender', "Choose your custom gender", {
+    [0] = "Male",
+    [1] = "Female",
+})
+UIDropDownMenu_SetWidth(customGenderDropdown, 100)
+
+-- And put them together:
 
 dressed:SetPoint("TOPLEFT", subText, "BOTTOMLEFT", 0, -8)
 uncover:SetPoint("TOPLEFT", dressed, "BOTTOMLEFT", 0, -4)
@@ -58,6 +148,11 @@ notifyKnown:SetPoint("TOPLEFT", uncover, "BOTTOMLEFT", 0, -4)
 currentClass:SetPoint("TOPLEFT", notifyKnown, "BOTTOMLEFT", 0, -4)
 mousescroll:SetPoint("TOPLEFT", currentClass, "BOTTOMLEFT", 0, -4)
 spin:SetPoint("TOPLEFT", mousescroll, "BOTTOMLEFT", 0, -4)
+
+modelBox:SetPoint("TOP", spin, "BOTTOM", 0, -20)
+customModel:SetPoint("LEFT", modelBox, 12, 0)
+customRaceDropdown:SetPoint("LEFT", customModel.Text, "RIGHT", 12, -2)
+customGenderDropdown:SetPoint("TOPLEFT", customRaceDropdown, "TOPRIGHT", 4, 0)
 
 InterfaceOptions_AddCategory(panel)
 
