f6bf4864f0f093f4e1ad9af99789a7312dde2150
David Lynch <kemayo@gmail.com>
2020-10-16 17:18:35 -0500

    Fix error checking for Inventorian when no AceAddon-using addon present
    Fixes #4

--- overlays.lua
+++ overlays.lua
@@ -148,7 +148,8 @@ end
 -- Other addons:
 
 -- Inventorian
-local inv = LibStub("AceAddon-3.0"):GetAddon("Inventorian", true)
+local AA = LibStub("AceAddon-3.0", true)
+local inv = AA and AA:GetAddon("Inventorian", true)
 if inv then
     hooksecurefunc(inv.Item.prototype, "Update", function(self, ...)
         UpdateContainerButton(self, self.bag)
